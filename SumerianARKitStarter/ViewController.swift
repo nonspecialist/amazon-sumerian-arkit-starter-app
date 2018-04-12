5770f549d32e634f13ac14703177a597c2a301d8
Colin Panisset <colin.panisset@cevo.com.au>
2018-04-12 16:52:49 +1000

    Try new scene

--- SumerianARKitStarter/ViewController.swift
+++ SumerianARKitStarter/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var sceneView: ARSCNView!
 
     // URL of the Sumerian scene.
-    private let sceneURL = URL(string: "https://d1550wa51vq95s.cloudfront.net/cab395b5e46b44f3affe4957fb04cf32.scene/?arMode=true")!
+    private let sceneURL = URL(string: "https://d1tsyd7599mesx.cloudfront.net/644c3120e0cb4e1aa174ab51a3a15fcf.scene/?arMode=true")!
 
     private var cubeMaterials: [SCNMaterial]!
     private var sumerianConnector: SumerianConnector!
